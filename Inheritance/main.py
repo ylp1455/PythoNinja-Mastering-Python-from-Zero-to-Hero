1436e5b7de2f6261e927ad68839cfe5137378de9
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-01 23:44:43 +0530

    .

--- Inheritance/main.py
+++ Inheritance/main.py
@@ -1,18 +1,26 @@
+#clases can have children.
+#Childern clasess will inherit from parent class.
+#childern classcan implement ther own and unqiue methods.
+
+
 class animal:
     alive = True
     def eat(self):
         print ("This animal is eating")
 
-    def sleep(self):
+    def slumber(self):
          print ("This animal is sleeping")
 
 class Rabbit(animal):
-    pass
+    def run(self):
+        print ("This rabbit is running")
 
 class Fish(animal):
-    pass
+    def swim(self):
+        print ("This fish is swiming")
 class Hawk(animal):
-    pass
+    def fly(self):
+        print ("This hawk is flying")
 
 rabbit = Rabbit()
 fish = Fish()
@@ -21,4 +29,9 @@ hawk = Hawk()
 print(rabbit.alive)
 
 fish.eat()
-hawk.sleep()
+hawk.fly()
+
+
+rabbit.run()
+fish.swim()
+hawk.fly()
