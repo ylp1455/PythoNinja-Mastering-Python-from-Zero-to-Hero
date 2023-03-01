c2dc52ee18bee378c17d0079da6b1db38c72767c
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-01 23:10:38 +0530

    .

--- Clasess/main.py
+++ Clasess/main.py
@@ -5,18 +5,28 @@
 car_1  = Car("Chevy" , "Corvette" , 2021 , "Blue")
 car_2  = Car("Ford" , "Mustang" , 2022 , "Red")
 
-#print(car_1.make)
-#print(car_1.model)
-#print(car_1.year)
-#print(car_1.color)
 
-#car_1.drive()
-#car_1.stop()
+print(car_1.make)
 
-#print(car_2.make)
-#print(car_2.model)
-#print(car_2.year)
-#print(car_2.color)
+print(car_1.model)
+
+print(car_1.year)
+
+print(car_1.color)
+
+
+car_1.drive()
+
+car_1.stop()
+
+
+print(car_2.make)
+
+print(car_2.model)
+
+print(car_2.year)
+
+print(car_2.color)
 
 car_2.drive()
 car_2.stop()
