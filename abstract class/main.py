1e2501b92c6635c2f83fd3423fc048e6714a9328
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-03 00:32:59 +0530

    .

---
+++ abstract class/main.py
@@ -0,0 +1,6 @@
+#prevent a user from creating an object of that class
+# + compels a user to override abstract methds in child class
+
+#abstract class = a class which contains one or more abstract methods
+#abstract method = a method that has a declaration but does not have an implementation
+
