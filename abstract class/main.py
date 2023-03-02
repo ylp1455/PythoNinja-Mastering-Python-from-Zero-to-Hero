d08d1b2c79adde6249f2ec2ea9a3ca9d02a072c6
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-03 00:45:05 +0530

    .

--- abstract class/main.py
+++ abstract class/main.py
@@ -4,3 +4,23 @@
 #abstract class = a class which contains one or more abstract methods
 #abstract method = a method that has a declaration but does not have an implementation
 
+class Vehical:
+    def go(self):
+        pass
+
+class Car(Vehicle):
+    def go(self):
+        print("You drive the car ")
+
+class Mortorcycle(Vehical):
+    def go(self):
+        print("You drive the mortorcycle ")
+
+
+vehical = Vehicle
+car = Car()
+mortorcycle = Mortorcycle()
+
+vehical.go()
+car.go()
+mortorcycle.go()
