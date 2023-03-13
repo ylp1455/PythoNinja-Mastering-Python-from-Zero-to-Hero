2026fa769df2a3254cb894c903e4a988a2f75480
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-13 14:53:36 +0530

    Sort Itterables

---
+++ Sort iterables/main.py
@@ -0,0 +1,51 @@
+#sort() method = used with list
+#sort() method = used with 
+
+# students = ["squidward" , "Sandy" , "Patric" , "Spongebob" ,"Mr.krabs"]
+
+# students.sort(reverse=True)
+
+# for i in students:
+#     print(i)
+
+
+# sorted_students = sorted(students,reverse=True)
+# for i in sorted_students:
+#     print(i)
+
+# -----------------------------------------------------------------------------------------------------------------
+
+students = [
+            
+            ("squidward", "F" , 60),
+            ("sandy" , "A" ,33),
+            ("Patrick" , "D" , 36),
+            ("Spongebob" , "B" ,20),
+            ("Mr.krabs" , "C" ,78)
+
+            ]
+# grade = lambda grade:grade[1]
+# students.sort(key=grade)
+# students.sort(key=grade)
+
+# for i in students:
+#     print(i)
+
+
+
+# grade = lambda grade:grade[1]
+# students.sort(key=grade , reverse=True)
+# for i in students:
+#     print(i)
+
+
+# age = lambda ages:ages[2]
+# students.sort(key=age, reverse=True)
+# for i in students:
+#     print(i)
+
+
+# age = lambda ages:ages[2]
+# sorted_students = sorted(students, key=age)
+# for i in students:
+#     print(i)
