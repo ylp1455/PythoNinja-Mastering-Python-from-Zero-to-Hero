cefe587fd91797d9fcad91928b14287d2a4b4d77
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-14 15:31:07 +0530

    function

---
+++ Map function/main.py
@@ -0,0 +1,11 @@
+#map() = applies a functio to each item an iterable(list,tuple,etc)
+#map(function,iterable)
+
+store = [
+
+        ("shirts",20.00),
+        ("pants",25.00),
+        ("Jackets",50.00),
+        ("Socks",10.00)
+
+]
