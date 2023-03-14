eb67522996beb57b1bbdb59580cb7e61bbd9c0d1
Yasiru Perera <yasiruperera682@gmail.com>
2023-03-14 17:08:12 +0530

    map function

--- Map function/main.py
+++ Map function/main.py
@@ -9,3 +9,18 @@ store = [
         ("Socks",10.00)
 
 ]
+
+to_euros = lambda data:(data[0],data[1]*0.82)
+to_dollars = lambda data:(data[0],data[1]/0.82)
+
+store_euros = list(map(to_euros ,store))
+
+for i in store_euros:
+    print(i)
+
+
+
+store_dolars = list(map(to_dollars ,store))
+
+for i in store_euros:
+    print(i)
