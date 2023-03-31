7ab0c83f138f4b03fb8530826e7e55eb877e2270
Yasiru Perera <yasiruperera682@gmail.com>
2023-04-01 01:05:00 +0530

    if __name__ varibales

--- if name ==/module_one.py
+++ if name ==/module_one.py
@@ -13,4 +13,27 @@
 # python interpreter sets "special variables" , one of which os __name
 # python will assign the __name__ variable a value of '__main__' if its the initial module being run
 
+# import module_two
+# print(__name__)
 
+# print(module_two.__name__)
+
+
+# if __name__ == '__main__':
+#     print("Running this module directly")
+
+# else :
+#     print ("Running other module indirectly")
+    
+
+
+def hello ():
+    print("Hello world")
+
+
+if __name__ == '__main__':
+    hello ()
+
+else :
+    print ("Running other module indirectly")
+    
