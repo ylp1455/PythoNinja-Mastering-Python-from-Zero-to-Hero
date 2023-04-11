540d6dd161976fe67c41cf385ea3dc5ce6b8c20f
Yasiru Perera <yasiruperera682@gmail.com>
2023-04-11 23:43:39 +0530

    create labels in gui

--- create labels in gui/main.py
+++ create labels in gui/main.py
@@ -1,13 +1,23 @@
 from tkinter import *
 
-# label = an area widget that holds text and/or an image wihin window
-
+# label = an area widget that holds text and/or an image within a window
 
 window = Tk()
-window.geometry("200x200")
 
-label =Label(window,  text="hello world")
+photo = PhotoImage(file='penguin.png')
+
+label = Label(window,
+              text="bro, do you even code?",
+              font=('Arial',40,'bold'),
+              fg='#00FF00',
+              bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound='bottom')
 label.pack()
-label.place(x = 0 , y = 0)
+#label.place(x=0,y=0)
 
 window.mainloop()
