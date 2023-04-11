32b5e5b4045fa227fd3cd3659a4c2b118f9bc480
Yasiru Perera <yasiruperera682@gmail.com>
2023-04-11 22:52:37 +0530

    tkinter first

---
+++ tkinter/main.py
@@ -0,0 +1,13 @@
+# widget  = GUI elements: buttons, labels,textboxes,image_names
+# windows = serve as a container to hold or contain theese widgets
+
+from tkinter import *
+
+
+window = Tk() #instantiate an instance of a window
+window.geometry("420x420")
+window.title("ylp first gui ")
+
+window.mainloop() #place windows on computer screen , listen for events
+
+
