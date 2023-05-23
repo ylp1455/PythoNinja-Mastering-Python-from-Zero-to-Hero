785a3dd7a18e06ddad2fe5d02ea7f5a28fc3a212
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-23 09:35:43 +0530

    2nd commit

--- Python GUI save a file (filedialog)/main.py
+++ Python GUI save a file (filedialog)/main.py
@@ -1,2 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+
+def saveFile():
+   file =  filedialog.asksaveasfile()
+   
+
+window = Tk()
+button = Button(text='save',command=saveFile)
+button.pack()
+text = Text(window)
+text.pack()
+window.mainloop()
