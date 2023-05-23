4f53984d5e2784e6fec2b18cccea10165155dfb1
Yasiru Perera <yasiruperera682@gmail.com>
2023-05-23 09:39:27 +0530

    finish project

--- Python GUI save a file (filedialog)/main.py
+++ Python GUI save a file (filedialog)/main.py
@@ -2,11 +2,21 @@ from tkinter import *
 from tkinter import filedialog
 
 def saveFile():
-   file =  filedialog.asksaveasfile()
-   
+    file = filedialog.asksaveasfile(initialdir="C:\\Users\\Cakow\\PycharmProjects\\Main",
+                                    defaultextension='.txt',
+                                    filetypes=[
+                                        ("Text file",".txt"),
+                                        ("HTML file", ".html"),
+                                        ("All files", ".*"),
+                                    ])
+    if file is None:
+        return
+    filetext = text.get(1.0, END)
+    file.write(filetext)
+    file.close()
 
 window = Tk()
-button = Button(text='save',command=saveFile)
+button = Button(window, text='Save', command=saveFile)
 button.pack()
 text = Text(window)
 text.pack()
